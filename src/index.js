92135965a3287dc92ddd4b8a48c271658b7da625
Diego Ezequiel Ceballos <diegoceballosdev@gmail.com>
2026-03-04 07:23:29 -0300

    Cambios para deploy de UI en vercel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT } from "./config.js";
 import userRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
